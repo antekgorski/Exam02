61b9010fa53a37a5838e1d117e044bdd0871d77c
Antoni Gorski <agorski@c1r8s4.42warsaw.pl>
2024-06-16 19:59:18 +0200

    tlist

--- Level 3/ft_list_size/ft_list_size.c
+++ Level 3/ft_list_size/ft_list_size.c
@@ -1,9 +1,24 @@
 #include "ft_list.h"
 
-int	ft_list_size(t_list *begin_list)
+// int	ft_list_size(t_list *begin_list)
+// {
+// 	if (begin_list == 0)
+// 		return (0);
+// 	else
+// 		return (1 + ft_list_size(begin_list->next));
+// }
+
+int		ft_list_size(t_list *begin_list)
 {
-	if (begin_list == 0)
-		return (0);
-	else
-		return (1 + ft_list_size(begin_list->next));
+	t_list *list;
+	int i;
+
+	list = begin_list;
+	i = 0;
+	while (list)
+	{
+		list = list->next;
+		i++;
+	}
+	return (i);
 }
